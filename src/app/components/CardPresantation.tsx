888887e24da937ce10988b0bf38c9ac40267179f
unknown <ouattara.abraham2019@gmail.com>
2024-08-06 08:40:33 +0100

    resolve deployyy

--- src/app/components/CardPresantation.tsx
+++ src/app/components/CardPresantation.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Btn from './Btn'
 import Image from 'next/image'
-export default function CardPresantation({props}) {
+export default function CardPresantation({props}:{props:any}) {
     
   return (
     <section>

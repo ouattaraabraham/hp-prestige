e45284d4730ed427b65271abd260d2d1fa7d4f6c
ouattaraabraham <110684159+ouattaraabraham@users.noreply.github.com>
2024-12-05 14:28:31 +0100

    Update Header.tsx
    change git color txt hearder page service

--- src/app/service/[id]/Header.tsx
+++ src/app/service/[id]/Header.tsx
@@ -7,7 +7,7 @@ export default function Header({props}:{props:any}) {
     <div className='relative overflow-hidden object-cover w-full lg:max-h-[500px]'>
         <Image className='' src={props.header.img} alt='menager'/>
         <div className='bg-black/60 flex items-center justify-center'>
-          <p className='absolute text-slate-950 top-0  mx-auto px-[.75rem] w-full h-full flex items-center justify-center font-bold text-slate-100/80 text-xl text-center md:text-6xl max-w-[1000px]'>
+          <p className='absolute text-white top-0  mx-auto px-[.75rem] w-full h-full flex items-center justify-center font-bold text-slate-100/80 text-xl text-center md:text-6xl max-w-[1000px]'>
           {props.header.p}
           </p>
         </div>
